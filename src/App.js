6320dd257939ba21e0330a9461b9c471427e6081
Rene Cabral <rene@oki.io>
2022-12-31 11:40:12 -0500

    Testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [value, setValue] = useState([1, 3]);
+
+  /*
+   * The second argument that will be passed to
+   * `handleChange` from `ToggleButtonGroup`
+   * is the SyntheticEvent object, but we are
+   * not using it in this example so we will omit it.
+   */
+  const handleChange = (val) => setValue(val);
+    return (
+    <ToggleButtonGroup type="checkbox" value={value} onChange={handleChange}>
+      <ToggleButton id="tbg-btn-1" value={1}>
+        Option 1
+      </ToggleButton>
+      <ToggleButton id="tbg-btn-2" value={2}>
+        Option 2
+      </ToggleButton>
+      <ToggleButton id="tbg-btn-3" value={3}>
+        Option 3
+      </ToggleButton>
+    </ToggleButtonGroup>
   );
 }
 
